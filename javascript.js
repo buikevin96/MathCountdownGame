36abcc23dd141ab457e950789be682033b725c26
buikevin96 <buikevin96@gmail.com>
2018-09-24 18:09:07 -0400

    Created startCountdown function

--- javascript.js
+++ javascript.js
@@ -1,5 +1,7 @@
 var playing = false; // Game starts at not playing
 var score;
+var action;
+var timeremaining;
 
 // When startReset button is clicked
 document.getElementById("startReset").onclick = function(){
@@ -14,8 +16,27 @@ document.getElementById("startReset").onclick = function(){
             document.getElementById("scorevalue").innerHTML = score;
             // Changes the display from none to block
             document.getElementById("timeRemaining").style.display = "block"; // Reveals the timer
-
+            timeremaining = 60;
+            document.getElementById("timeremainingvalue").innerHTML = timeremaining;
             document.getElementById("startReset").innerHTML = "Reset Game"; // Changes the startReset button text
 
+            startCountdown();
         }
+}
+
+function startCountdown(){
+    action = setInterval(function(){
+        timeremaining -= 1;
+
+        // Game Over
+        if(timeremaining == 0){
+            stopCountdown();
+            document.getElementById("gameOver").style.display = "block";
+            document.getElementById("gameOver").innerHTML = "<p>Game Over!</p><p>Your score is " + score + ".</p>";
+        }
+    }. 1000);
+}
+
+function stopCountdown(){
+    clearInterval(action);
 }
