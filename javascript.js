69b550f940e75f222277f36e3efeeb77078c35e8
buikevin96 <buikevin96@gmail.com>
2018-09-24 20:23:29 -0400

    Updated Boxes

--- javascript.js
+++ javascript.js
@@ -16,11 +16,16 @@ document.getElementById("startReset").onclick = function(){
             document.getElementById("scorevalue").innerHTML = score;
             // Changes the display from none to block
             document.getElementById("timeRemaining").style.display = "block"; // Reveals the timer
+            show("timeRemaining");
             timeremaining = 60;
             document.getElementById("timeremainingvalue").innerHTML = timeremaining;
+
+            //Hide Game over Box
+            hide("gameOver");
             document.getElementById("startReset").innerHTML = "Reset Game"; // Changes the startReset button text
 
             startCountdown();
+            generateQA();
         }
 }
 
@@ -28,15 +33,34 @@ function startCountdown(){
     action = setInterval(function(){
         timeremaining -= 1;
 
+        document.getElementById("timeremainingvalue").innerHTML = timeremaining;
         // Game Over
         if(timeremaining == 0){
             stopCountdown();
+            show("gameOver");
             document.getElementById("gameOver").style.display = "block";
             document.getElementById("gameOver").innerHTML = "<p>Game Over!</p><p>Your score is " + score + ".</p>";
+            hide("timeRemaining");
+            hide("correct");
+            hide("wrong");
+            playing = false;
+            document.getElementById("startReset").innerHTML = "Start Game";
         }
-    }. 1000);
+    }, 1000);
 }
 
 function stopCountdown(){
     clearInterval(action);
+}
+
+function hide(Id){
+    document.getElementById(Id).style.display = "none";
+}
+
+function show(Id){
+    document.getElementById(Id).style.display = "block";
+}
+
+function generateQA(){
+
 }
